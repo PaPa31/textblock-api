4ae7fe618cd89370abbdafb2be5c52251f450db1
papa31 <visidoor@yandex.ru>
2021-08-27 00:03:22 +0500

    feat: add time & date

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,6 +1,8 @@
-const Query = require('./query');
-const Mutation = require('./mutation');
-module.exports = {
+ const Query = require('./query');
+ const Mutation = require('./mutation');
+ const { GraphQLDateTime } = require('graphql-iso-date');
+ module.exports = {
     Query,
-    Mutation
-};
+    Mutation,
+    DateTime: GraphQLDateTime
+ };
