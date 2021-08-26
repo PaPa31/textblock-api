422f4d226d3d497ee77288c7a7da8c7675290dab
papa31 <visidoor@yandex.ru>
2021-08-26 23:41:21 +0500

    feat: add CRUD functionality

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,4 +1,27 @@
 module.exports = {
+    updateNote: async (parent, { content, id }, { models }) => {
+        return await models.Note.findOneAndUpdate(
+            {
+                _id: id,
+            },
+            {
+                $set: {
+                    content
+                }
+            },
+            {
+                new: true
+            }
+        );
+    },
+    deleteNote: async (parent, { id }, { models }) => {
+        try {
+            await models.Note.findOneAndRemove({ _id: id });
+            return true;
+        } catch (err) {
+            return false;
+        }
+ },
     newNote: async (parent, args, { models }) => {
         return await models.Note.create({
             content: args.content,
