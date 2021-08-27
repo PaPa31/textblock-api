39a7934c10f75db70af1aee01d63b077bbc7647e
papa31 <visidoor@yandex.ru>
2021-08-27 21:42:40 +0500

    feat: integration auth in api

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ const db = require('./db')
 const models = require('./models')
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const jwt = require('jsonwebtoken');
+
+// Получаем информацию пользователя из JWT
+const getUser = token => {
+    if (token) {
+        try {
+            // Возвращаем информацию пользователя из токена
+            return jwt.verify(token, process.env.JWT_SECRET);
+        } catch (err) {
+            // Если с токеном возникла проблема, выбрасываем ошибку
+            new Error('Session invalid');
+        }
+    }
+}
 
 // Запускаем сервер на порте, указанном в файле .env, или на порте 4000
 const port = process.env.PORT || 4000
@@ -17,15 +31,21 @@ const app = express();
 // Подключаем БД
 db.connect(DB_HOST)
 
- // Настройка Apollo Server
+// Настраиваем Apollo Server
 const server = new ApolloServer({
     typeDefs,
     resolvers,
- context: () => {
-     // Добавление моделей БД в context
-     return { models };
- }
- });
+    context: ({ req }) => {
+        // Получаем токен пользователя из заголовков
+        const token = req.headers.authorization;
+        // Пытаемся извлечь пользователя с помощью токена
+        const user = getUser(token);
+        // Пока что будем выводить информацию о пользователе в консоль:
+        console.log(user);
+        // Добавляем модели БД и пользователя в контекст 
+        return { models, user };
+    }
+})
 
 // Применяем промежуточное ПО Apollo GraphQL и указываем путь к /api
 server.applyMiddleware({ app, path: '/api' })
--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,4 +1,56 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {
+    AuthenticationError,
+    ForbiddenError
+} = require('apollo-server-express');
+
+require('dotenv').config();
+const gravatar = require('../util/gravatar');
+
 module.exports = {
+    signIn: async (parent, { username, email, password }, { models }) => {
+        if (email) {
+            // Нормализуем e-mail
+            email = email.trim().toLowerCase();
+        }
+        const user = await models.User.findOne({
+            $or: [{ email }, { username }]
+        });
+        // Если пользователь не найден, выбрасываем ошибку аутентификации
+        if (!user) {
+            throw new AuthenticationError('Error signing in');
+        }
+        // Если пароли не совпадают, выбрасываем ошибку аутентификации
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+            throw new AuthenticationError('Error signing in');
+        }
+        // Создаем и возвращаем json web token
+          return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    },
+    signUp: async (parent, { username, email, password }, { models }) => {
+        // Нормализуем имейл
+        email = email.trim().toLowerCase();
+        // Хешируем пароль
+        const hashed = await bcrypt.hash(password, 10);
+        // Создаем url gravatar-изображения
+        const avatar = gravatar(email);
+        try {
+            const user = await models.User.create({
+                username,
+                email,
+                avatar,
+                password: hashed
+            });
+            // Создаем и возвращаем json web token
+            return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        } catch (err) {
+            console.log(err);
+            // Если при регистрации возникла проблема, выбрасываем ошибку
+            throw new Error('Error creating account');
+        }
+    },
     updateNote: async (parent, { content, id }, { models }) => {
         return await models.Note.findOneAndUpdate(
             {
